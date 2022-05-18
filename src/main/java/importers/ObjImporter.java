99196306c08dafe400403e20cf3b67e1a6047c35
Michael Teschner <cujo2020jr@gmail.com>
2022-05-18 14:34:50 -0500

    basic obj importing

--- src/main/java/importers/ObjImporter.java
+++ src/main/java/importers/ObjImporter.java
@@ -5,122 +5,135 @@ import enums.BufferTypes;
 import graphics.Vbo;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.assimp.*;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ObjImporter{
-    public static Vao loadData(String path) {
-        FileReader fileReader = null;
-        try{
-            fileReader = new FileReader(new File("src/main/resources/" + path));
-        }catch (FileNotFoundException e){
-            System.err.println("could not find file: " + path);
-            e.printStackTrace();
-        }
-        BufferedReader reader = new BufferedReader(fileReader);
+    public static Vao loadData(String modelpath) {
+        //float[] positions {x, y, z, x, y, z}
+        //float[] texCoords {x, y, x, y}
+        //float[] normals {x, y, z}
+        //float[] colors {r, g, b, a}
 
-        String line;
+        ArrayList<Float> positions = new ArrayList<Float>();
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        ArrayList<Float> texCoords = new ArrayList<Float>();
+        ArrayList<Float> normals = new ArrayList<Float>();
+        ArrayList<Float> colors = new ArrayList<Float>();
 
-        List<Vector3f> vertices = new ArrayList<Vector3f>();
-        List<Vector3f> normals = new ArrayList<Vector3f>();
-        List<Vector2f> uvs = new ArrayList<Vector2f>();
-        List<Integer> indices = new ArrayList<Integer>();
+        String path = "src/main/resources/" + modelpath;
 
-        float[] vertexArray = null;
-        float[] normalArray = null;
-        float[] uvArray = null;
-        int[] indexArray = null;
+        loadModelFile(path, positions, indices, texCoords, normals, colors);
 
-        try{
-            while(true){
-                line = reader.readLine();
-                String[] currentLine = line.split(" ");
-                if(line.startsWith("v ")){
-                    Vector3f vertex = new Vector3f();
-                    vertex.x = Float.parseFloat(currentLine[1]);
-                    vertex.y = Float.parseFloat(currentLine[2]);
-                    vertex.z = Float.parseFloat(currentLine[3]);
-                    vertices.add(vertex);
-                }
-                else if(line.startsWith("vt ")){
-                    Vector2f uv = new Vector2f();
-                    uv.x = Float.parseFloat(currentLine[1]);
-                    uv.y = Float.parseFloat(currentLine[2]);
-                    uvs.add(uv);
-                }
-                else if(line.startsWith("vn ")){
-                    Vector3f normal = new Vector3f();
-                    normal.x = Float.parseFloat(currentLine[1]);
-                    normal.y = Float.parseFloat(currentLine[2]);
-                    normal.z = Float.parseFloat(currentLine[3]);
-                    normals.add(normal);
-                }
-                else if(line.startsWith("f ")){
-                    uvArray = new float[vertices.size() * 2];
-                    normalArray = new float[vertices.size() * 3];
-                    break;
-                }
-            }
+        float[] vertices = toFloatArray(positions);
+        int[] inds = toIntArray(indices);
+        float[] uvs = toFloatArray(texCoords);
 
-            while(line != null){
-                if(!line.startsWith("f ")){
-                    line = reader.readLine();
-                    continue;
-                }
-                String[] currentLine = line.split(" ");
-                String[] vertex1 = currentLine[1].split("/");
-                String[] vertex2 = currentLine[2].split("/");
-                String[] vertex3 = currentLine[3].split("/");
+        Vbo vertexBuffer = new Vbo(vertices, 3);
+        Vbo indexBuffer = new Vbo(inds, 1);
+        Vbo uvBuffer = new Vbo(uvs, 2);
 
-                processVertex(vertex1, indices, uvs, normals, uvArray, normalArray);
-                processVertex(vertex2, indices, uvs, normals, uvArray, normalArray);
-                processVertex(vertex3, indices, uvs, normals, uvArray, normalArray);
+        Vao array = new Vao();
 
-                line = reader.readLine();
-            }
-            reader.close();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        vertexArray = new float[vertices.size() * 3];
-        indexArray = new int[indices.size()];
+        array.uploadBuffer(vertexBuffer, BufferTypes.VERTEX_ARRAY_DATA);
+        array.uploadBuffer(indexBuffer, BufferTypes.INDEX_ARRAY_DATA);
+        array.uploadBuffer(uvBuffer, BufferTypes.UV_ARRAY_DATA);
 
-        int vertexPointer = 0;
-
-        for(Vector3f vertex:vertices){
-            vertexArray[vertexPointer++] = vertex.x;
-            vertexArray[vertexPointer++] = vertex.y;
-            vertexArray[vertexPointer++] = vertex.z;
-        }
-        for(int i = 0; i < indices.size(); i++){
-            indexArray[i] = indices.get(i);
-        }
-
-        Vbo vertexBuffer = new Vbo(vertexArray, 3);
-        Vbo indexBuffer = new Vbo(indexArray, 1);
-        Vbo uvBuffer = new Vbo(uvArray, 2);
-
-        Vao arrayBuffer = new Vao();
-
-        arrayBuffer.uploadBuffer(vertexBuffer, BufferTypes.VERTEX_ARRAY_DATA);
-        arrayBuffer.uploadBuffer(indexBuffer, BufferTypes.INDEX_ARRAY_DATA);
-        arrayBuffer.uploadBuffer(uvBuffer, BufferTypes.UV_ARRAY_DATA);
-
-        return arrayBuffer;
+        return array;
     }
 
-    private static void processVertex(String[] vertexData, List<Integer> indices, List<Vector2f> uvs, List<Vector3f> normals, float[] uvArray, float[] normalArray){
-        int currentVertexPointer = Integer.parseInt(vertexData[0]) - 1;
-        indices.add(currentVertexPointer);
-        Vector2f currentTex = uvs.get(Integer.parseInt(vertexData[1]) - 1);
-        uvArray[currentVertexPointer * 2] = currentTex.x;
-        uvArray[currentVertexPointer * 2 + 1] = currentTex.y;
-        Vector3f currentNorm = normals.get(Integer.parseInt(vertexData[2]) - 1);
-        normalArray[currentVertexPointer * 3] = currentNorm.x;
-        normalArray[currentVertexPointer * 3 + 1] = currentNorm.y;
-        normalArray[currentVertexPointer * 3 + 2] = currentNorm.z;
+    private static float[] toFloatArray(ArrayList<Float> arrList){
+        final float[] arr = new float[arrList.size()];
+        int index = 0;
+        for (final Float value: arrList) {
+            arr[index++] = value;
+        }
+        return arr;
+    }
+
+    private static int[] toIntArray(ArrayList<Integer> arrList){
+        final int[] arr = new int[arrList.size()];
+        int index = 0;
+        for (final Integer value: arrList) {
+            arr[index++] = value;
+        }
+        return arr;
+    }
+
+    private static void loadModelFile(String filepath, ArrayList<Float> positions, ArrayList<Integer> indices, ArrayList<Float> texCoords, ArrayList<Float> normals, ArrayList<Float> colors)
+    {
+        AIScene scene = Assimp.aiImportFile(filepath, Assimp.aiProcess_Triangulate);
+
+        System.out.println(scene.mMeshes().limit());System.out.println(scene.mNumMeshes());
+        PointerBuffer buffer = scene.mMeshes();
+
+        for(int i = 0; i < buffer.limit(); i++)
+        {
+            AIMesh mesh = AIMesh.create(buffer.get(i));
+            processMesh(mesh, positions, indices, texCoords, normals, colors);
+        }
+    }
+
+    private static void processMesh(AIMesh mesh, ArrayList<Float> positions, ArrayList<Integer> indices, ArrayList<Float> texCoords, ArrayList<Float> normals, ArrayList<Float> colors)
+    {
+        AIVector3D.Buffer vectors = mesh.mVertices();
+
+        for(int i = 0; i < vectors.limit(); i++)
+        {
+            indices.add(i);
+
+            AIVector3D vector = vectors.get(i);
+
+            positions.add(vector.x());
+            positions.add(vector.y());
+            positions.add(vector.z());
+        }
+
+        for(int i = 0; i < vectors.limit(); i++)
+        {
+            AIVector3D vector = vectors.get(i);
+
+            positions.add(vector.x());
+            positions.add(vector.y());
+            positions.add(vector.z());
+        }
+
+        AIVector3D.Buffer coords = mesh.mTextureCoords(0);
+
+        for(int i = 0; i < coords.limit(); i++)
+        {
+            AIVector3D coord = coords.get(i);
+
+            texCoords.add(coord.x());
+            texCoords.add(coord.y());
+        }
+
+        AIVector3D.Buffer norms = mesh.mNormals();
+
+        for(int i = 0; i < norms.limit(); i++)
+        {
+            AIVector3D norm = norms.get(i);
+
+            normals.add(norm.x());
+            normals.add(norm.y());
+            normals.add(norm.z());
+        }
+
+        AIColor4D.Buffer vertexColors = mesh.mColors(0);
+
+        if(vertexColors != null) {
+            for (int i = 0; i < vertexColors.limit(); i++) {
+                AIColor4D vertexColor = vertexColors.get(i);
+
+                colors.add(vertexColor.r());
+                colors.add(vertexColor.g());
+                colors.add(vertexColor.b());
+                colors.add(vertexColor.a());
+            }
+        }
     }
 }
